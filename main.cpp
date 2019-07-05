30a9360dcd8003cef403bff2cd6fc14fa23c1f09
Kostia Yeriomin <adaw36909@gmail.com>
2019-07-05 23:41:01 +0300

    add logic

--- main.cpp
+++ main.cpp
@@ -67,7 +67,70 @@ void CleanD3D(){
     devCon->Release();
 }
 
-int main(){
+// the entry point for any Windows program
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine,
+                   int nCmdShow) {
+
+    HWND hWnd;
+    WNDCLASSEX wc;
+
+    ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.lpszClassName = "WindowClass";
+
+    RegisterClassEx(&wc);
+
+    RECT wr = {0, 0, 800, 600};
+    AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+
+    hWnd = CreateWindowEx(NULL,
+                          "WindowClass",
+                          "Our First Direct3D Program",
+                          WS_OVERLAPPEDWINDOW,
+                          300,
+                          300,
+                          wr.right - wr.left,
+                          wr.bottom - wr.top,
+                          NULL,
+                          NULL,
+                          hInstance,
+                          NULL);
+
+    ShowWindow(hWnd, nCmdShow);
+
+    // set up and initialize Direct3D
+
+    InitD3D(hWnd);
+
+    // enter the main loop:
+
+    MSG msg;
+
+    while(TRUE)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+            if (msg.message == WM_QUIT){
+                break;
+            }
+        } else {
+            // Run game code here
+            // ...
+            // ...
+        }
+    }
+
+    // clean up DirectX and COM
     CleanD3D();
-    return 0;
+
+    return msg.wParam;
 }
