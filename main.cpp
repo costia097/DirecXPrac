4f0f730714743ff16604261d5adc5dc9e7449448
Kostia Yeriomin <adaw36909@gmail.com>
2019-07-06 11:58:24 +0300

    todo

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 #include <Windows.h>
-#include <windowsx.h>
 
 #include "include/dx11/D3D11.h"
 #include "include/dx11/D3DX11.h"
+#include "include/dx10/D3DX10math.h"
 
 #pragma comment (lib, "d3d11.lib")
 #pragma comment (lib, "d3dx11.lib")
@@ -13,25 +13,22 @@
 IDXGISwapChain *swapChain;
 ID3D11Device *dev;
 ID3D11DeviceContext *devCon;
+ID3D11RenderTargetView *backBuffer;
 
-//void InitD3D(HWND hWnd);
-//void CleanD3D();
-//LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "hicpp-multiway-paths-covered"
 // this is the main message handler for the program
-LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch(message)
-    {
-        case WM_DESTROY:
-        {
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
+    switch(message){
+        case WM_DESTROY: {
             PostQuitMessage(0);
             return 0;
-        } break;
+        }
     }
 
     return DefWindowProc (hWnd, message, wParam, lParam);
 }
+#pragma clang diagnostic pop
 
 void InitD3D(HWND hwnd){
     DXGI_SWAP_CHAIN_DESC scd;
@@ -44,6 +41,7 @@ void InitD3D(HWND hwnd){
     scd.OutputWindow = hwnd;
     scd.SampleDesc.Count = 4;
     scd.Windowed = true;
+
 
     D3D11CreateDeviceAndSwapChain(
             nullptr,
@@ -59,14 +57,50 @@ void InitD3D(HWND hwnd){
             nullptr,
             &devCon
     );
+    // get the address of the back buffer
+    ID3D11Texture2D *pBackBuffer;
+    swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+    // use the back buffer address to create the render target
+    dev->CreateRenderTargetView(pBackBuffer, nullptr, &backBuffer);
+    pBackBuffer->Release();
+
+    // set the render target as the back buffer
+    devCon->OMSetRenderTargets(1, &backBuffer, nullptr);
+
+    // Set the viewport
+    D3D11_VIEWPORT viewport;
+    ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+    viewport.TopLeftX = 0;
+    viewport.TopLeftY = 0;
+    viewport.Width = 800;
+    viewport.Height = 600;
+
+    devCon->RSSetViewports(1, &viewport);
+}
+
+// this is the function used to render a single frame
+void RenderFrame()
+{
+    // clear the back buffer to a deep blue
+    devCon->ClearRenderTargetView(backBuffer, D3DXCOLOR(0, 0.2f, 0.4f, 1.0f));
+
+    // do 3D rendering on the back buffer here
+
+    // switch the back buffer and the front buffer
+    swapChain->Present(0, 0);
 }
 
 void CleanD3D(){
     swapChain->Release();
+    backBuffer->Release();
     dev->Release();
     devCon->Release();
 }
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "hicpp-signed-bitwise"
 // the entry point for any Windows program
 int WINAPI WinMain(HINSTANCE hInstance,
                    HINSTANCE hPrevInstance,
@@ -82,7 +116,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
     wc.style = CS_HREDRAW | CS_VREDRAW;
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
     wc.lpszClassName = "WindowClass";
 
@@ -99,10 +133,10 @@ int WINAPI WinMain(HINSTANCE hInstance,
                           300,
                           wr.right - wr.left,
                           wr.bottom - wr.top,
-                          NULL,
-                          NULL,
+                          nullptr,
+                          nullptr,
                           hInstance,
-                          NULL);
+                          nullptr);
 
     ShowWindow(hWnd, nCmdShow);
 
@@ -114,14 +148,14 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
     MSG msg;
 
-    while(TRUE)
-    {
-        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+    while(TRUE){
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
             if (msg.message == WM_QUIT){
                 break;
             }
+            RenderFrame();
         } else {
             // Run game code here
             // ...
@@ -134,3 +168,4 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
     return msg.wParam;
 }
+#pragma clang diagnostic pop
